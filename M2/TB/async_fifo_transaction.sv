4b4dc4415e160948f49a17e39b2d49a8709e7510
GautamSavaliya3 <163203008+GautamSavaliya3@users.noreply.github.com>
2024-04-29 20:07:41 -0700

    Update async_fifo_transaction.sv

--- M2/TB/async_fifo_transaction.sv
+++ M2/TB/async_fifo_transaction.sv
@@ -1,34 +1,26 @@
 class transaction_write;
 	rand bit winc;
-	//rand bit rinc;
 	rand bit [11:0] wData;
-	//bit [11:0] rData;
 	bit wFull;
-	//bit rEmpty;
 	
 	function void display_write(string details1);
 		$display("----------------------");
 		$display("%s", details1);
 		$display("----------------------");
 		$display("winc = %d, wfull = %d, wData = %d",winc,wFull,wData);
-		//$display("rData = %d, rinc = %d, rEmpty = %d",rData,rinc,rEmpty);
 	endfunction
 endclass
 
 
 class transcation_read;
-	//rand bit winc;
 	rand bit rinc;
-	//rand bit [11:0] wData;
 	bit [11:0] rData;
-//	bit wFull;
 	bit rEmpty;
 	
 	function void display_read(string details2);
 		$display("----------------------");
 		$display("%s", details2);
 		$display("----------------------");
-		//$display("winc = %d, wfull = %d, wData = %d",winc,wFull,wData);
 		$display("rData = %d, rinc = %d, rEmpty = %d",rData,rinc,rEmpty);
 	endfunction
 endclass
