ddd38695213c7c171efb7aa2641cf6c69cb9baec
U-CECS\swaroopc <swaroopc@fab31.ds.cecs.pdx.edu>
2024-04-29 18:03:01 -0700

    pushing package file

---
+++ M2/TB/async_fifo_package.sv
@@ -0,0 +1,8 @@
+package fifo_pkg;
+	`include "async_fifo_transaction.sv"
+	`include "async_fifo_generator.sv"
+	`include "async_fifo_driver.sv"
+	`include "async_fifo_monitor.sv"
+	`include "async_fifo_scoreboard.sv"
+endpackage
+
