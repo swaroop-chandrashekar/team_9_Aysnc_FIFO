cf31acb797e7aed1571f690fcf00ffda720606c1
U-CECS\shashiki <shashiki@fab24.ds.cecs.pdx.edu>
2024-05-03 23:06:09 -0700

    top file fix

--- M2/TB/async_fifo_top.sv
+++ M2/TB/async_fifo_top.sv
@@ -7,44 +7,45 @@ Version:	1.0
 
   // Include the test.sv 
 `include "async_fifo_test.sv"
-
-`include "async_fifo_interface.sv"
+`include "aysnc_fifo_interface.sv"
 
 
 module async_fifo_top;
-
-bit rclk,wclk;
-bit rrst,wrst;
-
-always #4 wclk = ~wclk;
-always #10 rclk = ~rclk;
   
-//Instantiate the interface	
-intf DUV_IF (wclk,rclk,wrst,rrst);
-
-// Declare an handle for the test as testcase1
-test testcase1 (in);
-
-// instantiate the DUV
-AYNC_FIFO DUT (.wData(in.wData), .wFull(in.wFull), .rEmpty(in.rEmpty), .winc(in.winc), .rinc(in.rinc), .wclk(in.wclk), .rclk(in.rclk), .rrst(in.rrst), .wrst(in.wrst), .rData(in.rData));
+bit rclk,wclk,rrst,wrst;
   
+always #5 wclk = ~wclk;
+always #5 rclk = ~rclk;
   
-
 initial 
 begin
 	wclk =0;
     	rclk=0;
     	wrst =0;
     	rrst=0;
-    	in.rinc=0;
-    	in.winc=0;
-    	#1
+    	#10
     	rrst =1;
     	wrst=1;
-    
-
 end
+  
 
-   
+intf in (wclk,rclk,wrst,rrst);
+test t1 (in);
+
+ASYNC_FIFO DUT (.wData(in.wData),
+            .wFull(in.wFull),
+            .rEmpty(in.rEmpty),
+            .winc(in.winc),
+            .rinc(in.rinc),
+            .wclk(in.wclk),
+            .rclk(in.rclk),
+            .rrst(in.rrst),
+            .wrst(in.wrst),
+            .rData(in.rData));
+  
+  
+
+  
 endmodule
+
 
