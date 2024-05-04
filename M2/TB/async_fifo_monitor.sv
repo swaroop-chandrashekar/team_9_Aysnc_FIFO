cc1ee94b7ddbf829521fd0cd6ae8ca0e80b6c118
U-SMIT-PATEL\SMIT PATEL <smitdhi@pdx.edu>
2024-05-04 00:31:07 -0700

    updated monitor

--- M2/TB/async_fifo_monitor.sv
+++ M2/TB/async_fifo_monitor.sv
@@ -1,104 +1,44 @@
 //monitor is dynamic in nature so we use virtual interface 
 //handle is something when you don't give memory when you give memory then it
 //is called object........
+
 class monitor;
-int w_count;
-int r_count;
-int trans_count_write;
-int trans_count_read;
 
-	virtual intf vif;		//virtual interface declaration
-
-	mailbox mon2scb_write;		//declare mailbox for write
-	mailbox mon2scb_read;		//declare mailbox for read 
-
-	function new_read(virtual intf vif, mailbox mon2scb_read, mailbox mon2scb_write);
-		this.vif = vif;
-		this.mon2scb_read = mon2scb_read;
-		this.mon2scb_write = mon2scb_write;
-	endfunction
-
-	//sampling for write
-	task mon_write;
-		begin
-			transcation_write trans_w;
-			trans_w = new();			//constructor handle or creating object for trans
-			trans_W.winc =  vif.winc;
-			trans_w.wData = vif.wData;
-			trans_w.wFull = vif.wFull;
-			mon2scb_write.put(trans_w);
-			w_count = w_count + 1;
-			trans_w.display("display_write");
-		end
-	endtask
-	
-	//sampling for read
-	task mon_read;
-		begin
-			transcation_read trans_r;
-	       		trans_r = new();
-	 		trans_r.rinc = vif.rinc;
-			trans_r.rData = vif.rData;
-			trans_r.rEmpty = vif.rEmpty;
-			mon2scb_read.put(trans_r);
-			r_count = r_count +1;
-			trans_r.display("display_read");
-		end
-	endtask
-	
-
-	//main task used in the environment 
-	task main();
-		bit write_completed = 0;
-		transcation_read trans_read;
-		transcation_write trans_write;
-		trans_read = new();
-		trans_write = new();
-
-		fork
-										
-		begin : write_mon
-			forever @(posedge vif.wclk) begin
-			       mon_write();
-		       end
-	       	end
- 		
-		begin
-			forever @(posedge vif.wclk)
-				wait (w_count == trans_count_write);
-				disable write_mon
-				write_completed = 1;
-		end
-
-		join_any
-
-	if(write_completed == 1) 
+virtual intf mon_if;
+mailbox mon2scb;
+ 
+function new(virtual intf mon_if,mailbox mon2scb);
+	this.mon_if = mon_if;
+  	this.mon2scb = mon2scb;
+endfunction
+ 
+virtual task drive();
+begin
+     
+	transaction trans_mon;
+   	trans_mon = new(); 	  
+     	@(posedge mon_if.rclk);   
+    	trans_mon.rinc = mon_if.rinc;
+    	trans_mon.winc = mon_if.winc;
+    	trans_mon.wData = mon_if.wData;  
+    	trans_mon.wFull = mon_if.wFull;
+    	trans_mon.rEmpty = mon_if.rEmpty;
+    	trans_mon.rData = mon_if.rData; 
+    	mon2scb.put(trans_mon);
+end
+endtask
+    
+    
+    
+task  main();
+begin
+	for (int i = 0; i < 1; i++) 
 	begin
-		fork 
-			begin : read_mon
-				forever @(posedge vif.rclk)
-				begin
-	       			mon_read();
-				end
-			end
-
-			begin : completed_read
-				@(posedge vif.rclk)
-				wait(r_count == trans_count_read);
-				disable read_mon;
-			end
-		join_any
+        	drive(); 
 	end
-	
-	@(posedge vif.rclk);
- 	trans_read.rEmpty = vif.rEmpty;
-  	mon2scb_read.put(trans_read.rEmpty);
-  
-  	@(posedge vif.wclk);
-  	trans_write.wFull = vif.wFull;
-  	mon2scb_write.put(trans_write.wFull);
-			
-	endtask
+end
+endtask
 
 endclass
+  
  
