90710199401290b049646d2187dc95e9b6ce3f46
Swaroop Chandrashekar <swaroopc@mo.ece.pdx.edu>
2024-05-03 23:56:52 -0700

    updated test file

--- M2/TB/async_fifo_test.sv
+++ M2/TB/async_fifo_test.sv
@@ -1,30 +1,18 @@
-`include "async_fifo_environment.sv"
+import async_fifo_pkg::*;
 
 program test(intf in);
   environment env;
-  logic [5:0] read_request;
-  logic [5:0] write_request;
-  initial begin
-    $display("test environment start");
-    read_request = 5;
-    write_request = 5;
-    
+  
+initial begin
+  
+   $display("TEST START");
+        
     env = new(in);
-    env.gen.tx_count_read =5;
-    env.gen.tx_count_write =5;
+    env.gen.trans_count =100;    
+    env.no_of_transactions=100;
     
-    env.driv.trans_count_read=5;
-    env.driv.trans_count_write=5;
-    
-    env.mon.trans_count_write=5;
-    env.mon.trans_count_read=5;
-    
-    env.scb.trans_count_write=5;
-    env.scb.trans_count_read=5;
-
     env.run();
     $display("TEST FINISH");
     $finish;
   end
 endprogram
-
