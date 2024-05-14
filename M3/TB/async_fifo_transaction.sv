be4ffb0c03fcaa7f9e3526c4d6e2797f6fbace64
Swaroop Chandrashekar <swaroopc@mo.ece.pdx.edu>
2024-05-14 02:48:14 -0700

    updating the transaction file

--- M3/TB/async_fifo_transaction.sv
+++ M3/TB/async_fifo_transaction.sv
@@ -1,26 +1,17 @@
-class transaction_write;
-	rand bit winc;
-	rand bit [11:0] wData;
-	bit wFull;
-	
-	function void display_write(string details1);
-		$display("----------------------");
-		$display("%s", details1);
-		$display("----------------------");
-		$display("winc = %d, wfull = %d, wData = %d",winc,wFull,wData);
-	endfunction
-endclass
+class transaction;
+  	
+bit wrst, rrst, wclk, rclk;
+rand bit [11:0] wData;
+rand bit winc;
+rand bit rinc;
+
+//outputs 
+  	
+logic [11:0] rData;
+bit rEmpty;
+bit wFull;
+
+// Constraints
 
 
-class transcation_read;
-	rand bit rinc;
-	bit [11:0] rData;
-	bit rEmpty;
-	
-	function void display_read(string details2);
-		$display("----------------------");
-		$display("%s", details2);
-		$display("----------------------");
-		$display("rData = %d, rinc = %d, rEmpty = %d",rData,rinc,rEmpty);
-	endfunction
 endclass
